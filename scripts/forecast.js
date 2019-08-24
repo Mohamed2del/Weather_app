8f3ababe95aabb58040e15aadd4435e29f5d66f4
Mohamed2del <Mohamedadelmohamed997@gmail.com>
2019-08-24 05:35:07 +0200

    fix error

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,6 @@
 class Forecast {
     constructor(){
-      this.key = 'xvpOBAAypFh84YftzPvUCh8ZM80gbYIG';
+      this.key = 'vdDZ5w5l0NL5tB05zpZaGKsfRWYpvKq1';
       this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
       this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     }
